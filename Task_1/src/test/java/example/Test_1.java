3ba8d5e73470b664b848a2602786833a0cb16322
Albina <albinacneg55@gmail.com>
2025-03-19 00:44:08 +0300

    added first test

---
+++ Task_1/src/test/java/example/Test_1.java
@@ -0,0 +1,12 @@
+package example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class Test_1 {
+
+    @Test
+    public void testSimple() {
+        assertTrue(true);
+    }
+}
